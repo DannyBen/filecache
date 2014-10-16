880e6cb33d88db262bb517cccc4ef66c2073554a
Danny Ben Shitrit <dannyb@gmail.com>
2014-10-16 18:48:48 +0300

    Changed example to use temp folder, to work with Travis

--- filecache_test.go
+++ filecache_test.go
@@ -6,8 +6,9 @@ import (
 )
 
 func Example() {
-	// Get a handler and set a directory + 1 hour cache life
-	handler := filecache.Handler{"./cache", 1}
+	// Get a cache handler with default (temp) directory as
+	// the cache directory, and a one hour cache life
+	handler := filecache.Handler{Life: 1}
 
 	// Data to store in cache
 	data := []byte("Joey doesn't share food")
@@ -21,19 +22,6 @@ func Example() {
 	// Show the result
 	fmt.Println(string(r))
 
-	// Show the filename
-	fmt.Println(handler.Filename("testkey"))
-
-	// Wait for some seconds
-	// time.Sleep(7 * time.Second)
-
-	// By now the cache is invalid
-	// r = handler.Get("testkey")
-	// if r == nil {
-	// 	fmt.Println("Cache expired")
-	// }
-
 	// Output:
 	// Joey doesn't share food
-	// ./cache/221b368d7f5f597867f525971f28ff75
 }
